041937ad5c671efe12e06dee5fdbe2a184acfabe
Kevin Wang <wangxuzheng@aliyun.com>
2018-08-23 08:04:47 +0800

    oauth2工程中排除了对健康状态检查url的认证

--- pachira-oauth2/src/main/java/com/pachiraframework/oauth2/config/SecurityConfig.java
+++ pachira-oauth2/src/main/java/com/pachiraframework/oauth2/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -16,5 +17,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
+	}
+
+	@Override public void configure(WebSecurity web) throws Exception { 
+		//ignore 
+		web.ignoring().antMatchers("/actuator/**","/hystrix.stream"); 
 	}
 }
